87df25bc52d274a0ba664b933080d82c630d1c2b
Sofia Martinez <sofiadanielamartinez@gmail.com>
2023-04-21 19:19:29 -0300

    commit4

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -7,57 +7,33 @@ def base(request):
     return render(request, "AppCoder/base.html")
 
 def crear_usuario(request):
-    f = UsuarioForm(request.POST)
-    usuarios = Usuario.objects.all()
-    total_usuarios = len(usuarios)
-    context={
-        "usuarios": usuarios,
-        "total_usuarios": total_usuarios,
-        "form": f,
-    }
+     f = UsuarioForm(request.POST)
+     usuarios = Usuario.objects.all()
+     total_usuarios = len(usuarios)
+     context={
+         "usuarios": usuarios,
+         "total_usuarios": total_usuarios,
+         "form": f,
+     }
+   
+     if f.is_valid():
+         Usuario(nombre=f.data["nombre"], apellido=f.data["apellido"], dni=f.data["dni"]).save()
 
-    if f.is_valid():
-        Usuario(f.data["nombre"], f.data["apellido"], f.data["dni"]).save()
     
-    return render(request, "AppCoder/usuarios.html", context)
-
 def cargar_articulos(request):
-    f= ArticuloForm(request.POST)
-    if f.is_valid():
-        data = f.cleaned_data
-        articulo = Articulo(
-            producto= data["producto"],
-            marca= data["marca"],
-            cantidad= data["cantidad"]
-        )
-        articulo.save()
-        return redirect("articulos-create")
-    
-    f= ArticuloForm()
-    articulos = Articulo.objects.all()
-    total_articulos = len(articulos)
-    context={
-        "articulos": articulos,
-        "total_articulos": total_articulos,
-        "form": f
-    }
+     f = ArticuloForm(request.POST)
+     articulos = Articulo.objects.all()
+     total_articulos = len(articulos)
+     context={
+         "articulos": articulos,
+         "total_articulos": total_articulos,
+         "form": f,
+     }
 
-    return render(request, "AppCoder/articulos.html", context)
-    
-# def cargar_articulos(request):
-#     f = ArticuloForm(request.POST)
-#     articulos = Articulo.objects.all()
-#     total_articulos = len(articulos)
-#     context={
-#         "articulos": articulos,
-#         "total_articulos": total_articulos,
-#         "form": f,
-#     }
+     if f.is_valid():
+         Articulo(producto=f.data["producto"], marca=f.data["marca"], cantidad=f.data["cantidad"]).save()
 
-#     if f.is_valid():
-#         Articulo(f.data["producto"], f.data["marca"], f.data["cantidad"]).save()
-
-#     return render(request, "AppCoder/articulos.html", context)
+     return render(request, "AppCoder/articulos.html", context)
 
 
 
@@ -72,7 +48,8 @@ def envio_realizado(request):
     }
 
     if f.is_valid():
-        Envio(f.data["direccion"], f.data ["ciudad"], f.data ["provincia"], f.data ["cp"]).save()
+        Envio(direccion=f.data["direccion"], ciudad=f.data ["ciudad"], provincia=f.data ["provincia"], cp=f.data ["cp"]).save()
+        context ['form'] = EnvioForm()
     
     return render(request, "AppCoder/envios.html", context)
 
